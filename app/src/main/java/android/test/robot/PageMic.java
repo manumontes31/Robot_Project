da50c81b4536490dff6ea007c56577306c6dbc8b
Manu <montes131@gmail.com>
2016-11-09 23:31:40 +0100

    Test micro background

---
+++ app/src/main/java/android/test/robot/PageMic.java
@@ -0,0 +1,23 @@
+package android.test.robot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+/**
+ * Created by Manu on 09/11/2016.
+ */
+
+public class PageMic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mic);
+    }
+}
