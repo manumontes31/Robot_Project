f947507940f34949af9695399e6b07d36c06a7d3
blanchet.sebastien1@gmail.com <sebastien31>
2016-11-13 19:13:37 +0100

    commit Launcher avec slide

--- app/src/main/java/android/test/robot/TestLauncher.java
+++ app/src/main/java/android/test/robot/TestLauncher.java
@@ -1,12 +1,18 @@
 package android.test.robot;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import junit.framework.Test;
 
 public class TestLauncher extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -36,5 +42,36 @@ public class TestLauncher extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void saveSetting(View view) {
+        String sexe;
+
+        MediaPlayer mph = MediaPlayer.create(this,R.raw.pere);
+        MediaPlayer mpf = MediaPlayer.create(this,R.raw.maitre);
+
+        RadioGroup group_radio = (RadioGroup) findViewById(R.id.group_radio);
+        int checked = group_radio.getCheckedRadioButtonId();
+
+        RadioButton button1 = (RadioButton) findViewById(R.id.radio1);
+        int idB1 = button1.getId();
+
+        RadioButton button2 = (RadioButton) findViewById(R.id.radio2);
+        int idB2 = button2.getId();
+
+        if (checked == idB1) {
+            sexe = "Homme";
+            mph.start();
+        }
+
+        if (checked == idB2) {
+            sexe = "Femme";
+            mpf.start();
+        }
+
+        if((!mph.isPlaying()) || (!mpf.isPlaying())) {
+            Intent i = new Intent(TestLauncher.this, MainActivity.class);
+            startActivity(i);
+        }
     }
 }
