37fe92b090e6358eff64fba61e7aa1dbc3d98244
Manu <montes131@gmail.com>
2016-11-14 23:21:20 +0100

    Ajout page début app Test Lecture sans succes

--- app/src/main/java/android/test/robot/Lecture.java
+++ app/src/main/java/android/test/robot/Lecture.java
@@ -2,13 +2,11 @@ package android.test.robot;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 import android.widget.RadioButton;
+
+import static android.media.MediaPlayer.create;
 
 public class Lecture extends AppCompatActivity {
 
@@ -16,37 +14,48 @@ public class Lecture extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lecture);
-
     }
 
     public void launchMusic(View view) {
         boolean checked = ((RadioButton) view).isChecked();
-        MediaPlayer mp;
+        MediaPlayer mpC = create(this, R.raw.chewbacca);
+        MediaPlayer mpIM = create(this,R.raw.imperial_march);
+        MediaPlayer mpM = create(this, R.raw.maitre);
+        MediaPlayer mpP = create(this, R.raw.pere);
 
         switch(view.getId()) {
             case R.id.radioChewbacca:
-                if (checked) {
-                    mp = MediaPlayer.create(this, R.raw.chewbacca);
-                    mp.start();
-                }
+                if (checked && (mpIM.isPlaying() || mpM.isPlaying() || mpP.isPlaying())) {
+                    mpIM.stop();
+                    mpM.stop();
+                    mpP.stop();
+                } else
+                    mpC.start();
                 break;
             case R.id.radioIMarch:
-                if (checked) {
-                    mp = MediaPlayer.create(this,R.raw.imperial_march);
-                    mp.start();
-                }
+                if (checked && (mpC.isPlaying() || mpM.isPlaying() || mpP.isPlaying())) {
+                    mpM.stop();
+                    mpP.stop();
+                    mpC.stop();
+                } else
+                    mpIM.start();
                 break;
             case R.id.radioMaitre:
-                if (checked) {
-                    mp = MediaPlayer.create(this, R.raw.maitre);
-                    mp.start();
-                }
+                if (checked && (mpC.isPlaying() || mpIM.isPlaying() || mpP.isPlaying())) {
+                    mpP.stop();
+                    mpC.stop();
+                    mpIM.stop();
+                } else
+                    mpM.start();
+
                 break;
             case R.id.radioPere:
-                if (checked) {
-                    mp = MediaPlayer.create(this, R.raw.pere);
-                    mp.start();
-                }
+                if (checked && (mpC.isPlaying() || mpIM.isPlaying() || mpC.isPlaying())) {
+                    mpC.stop();
+                    mpIM.stop();
+                    mpM.stop();
+                } else
+                    mpP.start();
                 break;
         }
     }
---
+++ app/src/main/java/android/test/robot/SplashScreen.java
@@ -0,0 +1,37 @@
+package android.test.robot;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Manu on 14/11/2016.
+ */
+
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,TestLauncher.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
